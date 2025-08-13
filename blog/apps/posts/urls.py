07d38bc79e209810927cc1f18848c8c0f9d48eb8
David <davidbritto54.com>
2025-08-12 23:50:14 -0300

    Feat: agregando Carrusel, uploader de imagenes, entre otros features

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import index, PostDetailView, crear_post
+from .upload_views import upload_image
 
 urlpatterns = [
     path('', index, name='post_list'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post_detail'),
-    path('nuevo/', crear_post, name='crear_post'),  # ← NUEVA RUTA
+    path('nuevo/', crear_post, name='crear_post'),
+    path('upload/', upload_image, name='upload_image'),
 ]
