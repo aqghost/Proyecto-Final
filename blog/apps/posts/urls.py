b146594e27705d194f8ff78bbfea86895acf6a57
aqghost <acondeolgado@gmail.com>
2025-08-15 06:15:25 -0300

    Añadido sistema de permisos basado en grupos, sistema de likes simples, posibilidad de editar y eliminar comentarios, página de contactos, mejora de la sección de categorias (ahora se pueden ordenar los posts dentro de la sección)

--- blog/apps/posts/urls.py
+++ blog/apps/posts/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path
-from .views import index, PostDetailView, CategoryListView, crear_post, EditPostView, DeletePostView, PostView
+from .views import index, PostDetailView, CategoryListView, crear_post, EditPostView, DeletePostView, PostView, PostCommentDeleteView, PostCommentEditView, LikeView, ContactView
 from .upload_views import upload_image
 
 urlpatterns = [
     path('', index, name='index'),
-    path('post/<int:pk>', PostView.as_view(), name='post_detail'),
+    path('post/<int:pk>/', PostView.as_view(), name='post_detail'),
     path('categoria/<str:nombre>', CategoryListView.as_view(), name='categorias'),
     path('nuevo/', crear_post, name='crear_post'),
     path('upload/', upload_image, name='upload_image'),
-    path('post/editar/<int:pk>', EditPostView.as_view(), name="editar_post"),
-    path('post/eliminar/<int:pk>', DeletePostView.as_view(), name="eliminar_post"),
+    path('post/editar/<int:pk>/', EditPostView.as_view(), name='editar_post'),
+    path('post/eliminar/<int:pk>/', DeletePostView.as_view(), name='eliminar_post'),
+    path('post/<int:post_pk>/eliminar-comentario/<int:pk>/', PostCommentDeleteView.as_view(), name='eliminar_comentario'),
+    path('post/<int:post_pk>/editar-comentario/<int:pk>/', PostCommentEditView.as_view(), name='editar_comentario'),
+    path('like/<int:pk>', LikeView, name='like_post'),
+    path('contacto/', ContactView, name='contactar')
 ]
--- blog/apps/usuario/models.py
+++ blog/apps/usuario/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
 
+    def es_colaborador(self):
+        return self.groups.filter(name="Colaborador").exists()
+    
     def __str__(self):
         return f'{self.last_name}, {self.first_name}'
 
